4fdb7272dcf40075aadf8a1409967109a91a22a5
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-02-28 07:08:59 +0300

    Add jquery library

---
+++ designs/UI/js/index.js
@@ -0,0 +1,13 @@
+$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  }
+);
+
+// $('.dropdown-button').dropdown('open');
