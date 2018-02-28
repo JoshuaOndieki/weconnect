c175b2a7533a76e40d644757e7b33397584b5bbb
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-03-01 02:34:05 +0300

    Create business listing page

--- designs/UI/js/index.js
+++ designs/UI/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
+  $('.dropdown-button').dropdown('open');
+  $('select').material_select();
 });
-
-$('.dropdown-button').dropdown('open');
