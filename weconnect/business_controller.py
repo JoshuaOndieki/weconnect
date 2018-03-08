e461e927a671e4f3dac2b5348be62c71dd50c0db
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-03-08 10:26:56 +0300

    Create edit business functiona;lity

--- weconnect/business_controller.py
+++ weconnect/business_controller.py
@@ -26,41 +26,37 @@ class BusinessController():
         except Exception as e:
             return (False, str(e))
 
-    def edit(self, business_id, name=None, location=None, category=None):
+    def edit(self, data):
         """
             Edits and updates business info.
 
-            Returns:
-                A tuple of (True, name, location, category) if success updating business, (False, error) otherwise.
-        """
-
-        pass
-
-    def retrieve_business(self, bs_id=None, user_id=None, location=None, category=None):
-        """
-            Retrieves business/es. Filters if given filter options.
+            Arguments:
+                data: A dictionary containing
+                        business_id, name, location, category
 
             Returns:
-                A tuple of (True, {business_id: [name, location, category, user_id]}) if success retrieving business/es, (False, error) otherwise.
+                A tuple of (True, name, location, category) if success
+                updating business, (False, error) otherwise.
         """
+
         try:
-            if not user_id and not location and not category:
-                # retrieve all business if no filter parameters are specified.
-                return (True, app.database['Businesses'])
-            else:
-                # if filter parameters are given, return a filtered result.
-                if bs_id:
-                    return (True, app.database['Businesses'][bs_id])
-                # if user_id:
-                #     if location:
-                #         if category:
-                #             # filter with all parameters
-                #             all_businesses = app.database['Businesses']
-                #             businesses = [x for x in all_businesses if x[1] == location and x[2] == category and x[3] == user_id]
-                #             return (True, businesses)
-                #             ...
+            if data['id'] in app.database['Businesses']:
+                # Update business
+                business = app.database['Businesses'][data['id']]
+                business[0] = data['name']  # Update Name
+                business[1] = data['location']  # Update Location
+                business[2] = data['category']  # Update Category
+                app.database['Businesses'][data['id']] = business
+                return (True, 'Business update successful!')
+            return (False, 'No business found with given id!')
         except Exception as e:
             return (False, str(e))
+
+    def get_businesses(self):
+        pass
+
+    def get_business_by_id(self):
+        pass
 
     def delete_business(self, business_id):
         """
